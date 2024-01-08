17e08f26d4e13ec3094f0e32bd9a11715fac698a
Luthfi Solahudin <54571802+luthfisolahudin@users.noreply.github.com>
2024-01-08 07:51:42 +0700

    refactor: add tailwindcss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.min.css') }}">
+    @vite('resources/css/app.css')
     @stack('style')
 
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}" async></script>
-    <script src="{{ asset('vendor/tailwindcss/index.js') }}" defer></script>
 
     <title>Hotel Booking</title>
 </head>
