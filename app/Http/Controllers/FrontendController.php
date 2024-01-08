2e9238a1ec169bc321a636bb2f1a965859806467
Luthfi Solahudin <54571802+luthfisolahudin@users.noreply.github.com>
2024-01-08 12:59:49 +0700

    refactor: cleanup controller

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FrontEndController extends Controller
+class FrontendController extends Controller
 {
     public function index()
     {
